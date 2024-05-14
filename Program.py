011637163aa16440df0f9fcf1b5ed0bd19f6ade4
Jonathan Thomassen <jt@exopi.dk>
2024-05-14 10:54:09 +0200

    Added capability for resetting PDF library

--- Program.py
+++ Program.py
@@ -1,20 +1,32 @@
 import requests
 import csv
 import datetime
+import sys
+import shutil
+import pathlib
 
 from requests.exceptions import ConnectionError, RetryError
 from requests.adapters import HTTPAdapter, Retry
 from datetime import datetime
+from ssl import SSLCertVerificationError
+
+argument = ''
+if (len(sys.argv) > 1):
+    argument = sys.argv[1]
 
 urlDict = {}
 resultList = []
 resultsCsvName = 'Results_' + datetime.now().strftime('%Y%m%d%H%M%S') + '.csv'
 
+if (argument == '-r'):
+    print('Reset requested. Deleting all files in PDFs folder...')
+    shutil.rmtree("./PDFs/")
+    pathlib.Path.mkdir("PDFs")
+
 with open("GRI_2017_2020.csv", newline="", errors="ignore") as csvFile:
     csvReader = csv.DictReader(csvFile, delimiter=",")
     id = 1
     for row in csvReader:
-        print(row["pdf_url_1"])
         urlDict.update({id: {row['pdf_url_1'], row['pdf_url_2']}})
         id += 1
 
@@ -44,28 +56,35 @@ for row in urlDict:
             localFilename = str(row) + '_' + url.split('/')[-1]
             try:
                 # Acquire response from server
-                response = session.get(url, verify=False)
-            except (ConnectionError, RetryError) as e:
+                response = session.get(url)
+            except (ConnectionError, RetryError):
                 print('The following file could not be downloaded (connection error): ' + url)
                 resultList[row-1] += [url, 'Error: File could not be downloaded (connection error).']
                 pass
             else:
-                print(response.headers['Content-Type'])
-                if (response.headers['Content-Type'] == 'application/pdf'):
-                    try:
-                        # Write to PDF file
-                        with open('./PDFs/' + localFilename, 'xb') as f:
-                            f.write(response.content)
-                        print('Successfully downloaded: ' + url)
-                        resultList[row-1] += [url, 'Succesfully downloaded.']
-                        break
-                    except FileExistsError:
-                        print('File already exists: ' + localFilename)
-                        resultList[row-1] += [url, 'Error: File already exists.']
-                        break
+                try:
+                    print(response.headers['Content-Type'])
+                except KeyError:
+                    print('Response from server does not contain a \'Content-Type\' field: ' + url)
+                    resultList[row-1] += [url, 'Error: Response from server does not contain a \'Content-Type\' field.']
+                    pass
                 else:
-                    print('File linked to in URL is not a PDF document: ' + url)
-                    resultList[row-1] += [url, 'Error: File linked to in URL is not a PDF document.']
+                    if (response.headers['Content-Type'] == 'application/pdf'):
+                        try:
+                            # Write to PDF file
+                            with open('./PDFs/' + localFilename, 'xb') as f:
+                                f.write(response.content)
+                        except FileExistsError:
+                            print('File already exists: ' + localFilename)
+                            resultList[row-1] += [url, 'Error: File already exists.']
+                            break
+                        else:
+                            print('File successfully downloaded: ' + url)
+                            resultList[row-1] += [url, 'File successfully downloaded.']
+                            break
+                    else:
+                        print('File linked to in URL is not a PDF document: ' + url)
+                        resultList[row-1] += [url, 'Error: File linked to in URL is not a PDF document.']
         else:
             print('Hyperlink not a valid URL to a PDF document: ', end='')
             if (url != ''):
