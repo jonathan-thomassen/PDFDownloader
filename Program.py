64ad65d8e34b4cb54ebcb7aee91b18df46e0543e
Jonathan Thomassen <jt@exopi.dk>
2024-05-15 14:51:42 +0200

    Added MD5 validation of results

--- Program.py
+++ Program.py
@@ -2,19 +2,20 @@ import requests
 import csv
 import datetime
 import sys
+import hashlib
+import pathlib
 
 from requests.exceptions import ConnectionError, RetryError
 from requests.adapters import HTTPAdapter, Retry
 from datetime import datetime
-from ssl import SSLCertVerificationError
 from pathlib import Path
 from UrlEntry import UrlEntry
 from ResultEntry import ResultEntry
 
-# Currently missing: 69
-# TODO: Lav klasser af listerne. 
+# Currently missing: 128: Sends to script which redirects to PDF? Maybe Google Chrome magic is what makes it work?
 
 urlListCsvPath = './GRI_2017_2020.csv'
+md5ListCsvPath = './GRI_2017_2020_MD5_20220515.csv'
 pdfWriterArg = 'xb'
 overwrite = False
 
@@ -29,10 +30,12 @@ session.mount('https://', HTTPAdapter(max_retries=retries))
 httpHeaders = {
     "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
     "Accept-Encoding": "gzip, deflate, br, zstd",
+    "Accept-Language": "en-DK,en;q=0.9,da-DK;q=0.8,da;q=0.7,en-US;q=0.6",
     "Cache-Control": "no-cache",
+    "Connection": "keep-alive",
     "Dnt": "1",
     "Pragma": "no-cache",
-    "Sec-Ch-Ua": "\"Chromium\";v=\"124\", \"Google Chrome\";v=\"124\", \"Not-A.Brand\";v=\"99\"",
+    "Upgrade-Insecure-Requests": "1",
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36"
 }
 
@@ -107,7 +110,6 @@ def downloadPdfs():
                 try:
                     # Acquire response from server
                     print('Id: ' + urlEntry.id + '. Sending \'GET\' request: ' + url)
-                    # TODO: Prøv at lege med timeout værdier
                     response = session.get(url, timeout=(6.05, 120), verify=False, headers=httpHeaders)
                 # TODO: Læs op på session og ConnectionError
                 except (ConnectionError, RetryError):
@@ -124,9 +126,10 @@ def downloadPdfs():
                     else:
                         if ('application/pdf' in contentType):
                             content = response.content
-                            if (sys.getsizeof(content) > 0):
+                            if (sys.getsizeof(content) > 33):
                                 writePdfToFile(content, localFilename, url, urlEntry.id)
                                 break
+                        print(response.content)
                         print('Id: ' + urlEntry.id + '. File linked to in URL is not a PDF document: ' + url)
                         resultList[-1].AddResult(url, 'Error: File linked to in URL is not a PDF document.')                   
             else:
@@ -139,5 +142,36 @@ def downloadPdfs():
                     resultList[-1].AddResult(url, 'Error: URL blank.')
         writeResultsCsv()
 
+def validateResults(filePath, delimiter, quotechar):
+    with open(filePath, newline="") as csvFile:
+        csvReader = csv.reader(csvFile, delimiter=delimiter, quotechar=quotechar)
+        md5Dict = {}
+        n = 0
+        for row in csvReader:
+            if (n > 0):
+                md5Dict.update({row[0]: row[1]})
+            n = 1
+
+    path = pathlib.Path('./ActualPDFs/')    
+    files = [f for f in path.iterdir() if f.is_file()]
+
+    valSuccess = 0
+    valFailed = 0
+
+    for file in files:
+        if (file.suffix.upper() == '.PDF'):
+            id = file.name.split('_')[0]
+            with open('./ActualPDFs/' + file.name, 'rb') as pdf:
+                md5hash = hashlib.md5(pdf.read()).hexdigest()
+                if (md5hash == md5Dict[id].lower()):
+                    print('Id: ' + id + '. Successful validation.')
+                    valSuccess += 1
+                else:
+                    print('Id: ' + id + '. Validation failed. MD5 hash mismatch.')
+                    valFailed += 1
+    
+    print('Validation results: ' + str(valSuccess) + ' files succeeded. ' + str(valFailed) + ' files failed.')
+
 readUrlListCsv(urlListCsvPath, ',', '\"')
 downloadPdfs()
+validateResults(md5ListCsvPath,',', '\"')
