430a447c819e2c0622419ed604875f25a08fa4e4
Jonathan Thomassen <jt@exopi.dk>
2024-05-15 10:15:11 +0200

    Refactoring. Checking if content response is larger than 0

--- Program.py
+++ Program.py
@@ -9,39 +9,21 @@ from datetime import datetime
 from ssl import SSLCertVerificationError
 from pathlib import Path
 
-argument = ''
+# Currently missing: 69
+# TODO: 104 is erronously downloaded. Lav klasser af listerne. 
+
+urlListCsvPath = './GRI_2017_2020.csv'
 pdfWriterArg = 'xb'
 overwrite = False
 
-if (len(sys.argv) > 1):
-    argument = sys.argv[1]
-    if (argument == '-r'):
-        print('Reset requested. Downloader will overwrite old files.')
-        pdfWriterArg = 'wb'
-        overwrite = True
-
 urlList = []
 resultList = []
+
 resultsCsvName = 'Results_' + datetime.now().strftime('%Y%m%d%H%M%S') + '.csv' 
-
-with open("GRI_2017_2020.csv", newline="", errors="ignore") as csvFile:
-    csvReader = csv.reader(csvFile, delimiter=",", quotechar="|")
-    n = 0
-    for row in csvReader:
-        if (n > 0):
-            urlList.append([row[0]])
-            urlAB = []
-            for element in row[1:]:
-                urlAB.append(element)
-
-            urlList[-1].append(urlAB)
-        n += 1
-
 session = requests.Session()
 retries = Retry(total=5, backoff_factor=0.1)
 session.mount('http://', HTTPAdapter(max_retries=retries))
 session.mount('https://', HTTPAdapter(max_retries=retries))
-
 httpHeaders = {
     "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
     "Accept-Encoding": "gzip, deflate, br, zstd",
@@ -51,6 +33,27 @@ httpHeaders = {
     "Sec-Ch-Ua": "\"Chromium\";v=\"124\", \"Google Chrome\";v=\"124\", \"Not-A.Brand\";v=\"99\"",
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36"
 }
+
+if (len(sys.argv) > 1):
+    argument = sys.argv[1]
+    if (argument != None and argument == '-o'):
+        print('Overwrite flag set. Downloader will overwrite old files.')
+        pdfWriterArg = 'wb'
+        overwrite = True
+
+def readUrlListCsv(filePath, delimiter, quotechar) :
+    with open(filePath, newline="", errors="ignore") as csvFile:
+        csvReader = csv.reader(csvFile, delimiter=delimiter, quotechar=quotechar)
+        n = 0
+        for row in csvReader:
+            if (n > 0):
+                urlList.append([row[0]])
+                urlAB = []
+                for element in row[1:]:
+                    urlAB.append(element)
+
+                urlList[-1].append(urlAB)
+            n += 1
 
 def writeResultsCsv():
     with open(resultsCsvName, 'w', newline='') as csvFile:
@@ -66,36 +69,23 @@ def writeResultsCsv():
 
             csvWriter.writerow(csvRow)
 
-def writePdfToFile(response, localFilename, url, id):
+def writePdfToFile(content, localFilename, url, id):
     try:
-        response.headers['Content-Type']
-    except KeyError:
-        print('Id: ' + id + '. Response from server does not contain a \'Content-Type\' field: ' + url)
-        resultList[-1] += [url, 'Error: Response from server does not contain a \'Content-Type\' field.']
-        pass
+        # Write to PDF file
+        with open('./PDFs/' + localFilename, pdfWriterArg) as f:
+            f.write(content)
+    except FileExistsError:
+        print('Id: ' + id + '. File already exists: ' + localFilename)
+        resultList[-1] += [url, 'Error: File already exists.']
     else:
-        if (response.headers['Content-Type'] == 'application/pdf'):
-            try:
-                # Write to PDF file
-                with open('./PDFs/' + localFilename, pdfWriterArg) as f:
-                    f.write(response.content)
-            except FileExistsError:
-                print('Id: ' + id + '. File already exists: ' + localFilename)
-                resultList[-1] += [url, 'Error: File already exists.']
-            else:
-                print('Id: ' + id + '. File successfully downloaded: ' + url)
-                resultList[-1] += [url, 'File successfully downloaded.']
-            finally:
-                return True
-        else:
-            print('Id: ' + id + '. File linked to in URL is not a PDF document: ' + url)
-            resultList[-1] += [url, 'Error: File linked to in URL is not a PDF document.']
-    return False
+        print('Id: ' + id + '. File successfully downloaded: ' + url)
+        resultList[-1] += [url, 'File successfully downloaded.']
 
 def downloadPdfs():
     for row in urlList:
         resultList.append([row[0]])
         for url in row[1]:
+            # TODO: Eventuelt lav check om til funktion
             if (url.upper().startswith('HTTP')):
                 if (url.upper().startswith('HTTP:')):
                     url = 'https:' + url[5:]
@@ -103,6 +93,7 @@ def downloadPdfs():
                 if (localFilename.upper().endswith('.PDF') == False):
                     localFilename += '.pdf'
                 if (overwrite == False):
+                    # TODO: Lav til funktion
                     path = Path('./PDFs/' + localFilename)
                     if (path.is_file()):
                         print('Id: ' + row[0] + '. File already exists: ' + localFilename)
@@ -111,14 +102,28 @@ def downloadPdfs():
                 try:
                     # Acquire response from server
                     print('Id: ' + row[0] + '. Sending \'GET\' request: ' + url)
-                    response = session.get(url, timeout=(3.05, 120), verify=False, headers=httpHeaders)
+                    # TODO: Prøv at lege med timeout værdier
+                    response = session.get(url, timeout=(6.05, 120), verify=False, headers=httpHeaders)
+                # TODO: Læs op på session og ConnectionError
                 except (ConnectionError, RetryError):
                     print('Id: ' + row[0] + '. The following file could not be downloaded (connection error): ' + url)
                     resultList[-1] += [url, 'Error: File could not be downloaded (connection error).']
                     pass
                 else:
-                    if writePdfToFile(response, localFilename, url, row[0]):
-                        break
+                    try:
+                        contentType = response.headers['Content-Type']
+                    except KeyError:
+                        print('Id: ' + row[0] + '. Response from server does not contain a \'Content-Type\' field: ' + url)
+                        resultList[-1] += [url, 'Error: Response from server does not contain a \'Content-Type\' field.']
+                        pass
+                    else:
+                        if (contentType == 'application/pdf'):
+                            content = response.content
+                            if (sys.getsizeof(content) > 0):
+                                writePdfToFile(content, localFilename, url, row[0])
+                                break
+                        print('Id: ' + row[0] + '. File linked to in URL is not a PDF document: ' + url)
+                        resultList[-1] += [url, 'Error: File linked to in URL is not a PDF document.']                    
             else:
                 print('Id: ' + row[0] + '. Hyperlink not a valid URL to a PDF document: ', end='')
                 if (url != ''):
@@ -129,4 +134,5 @@ def downloadPdfs():
                     resultList[-1] += [url, 'Error: URL blank.']
         writeResultsCsv()
 
+readUrlListCsv(urlListCsvPath, ',', '|')
 downloadPdfs()
